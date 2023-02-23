1341ce461da107fdea30a2dcafb44f6fa79e24f4
Wendesday36 <bertazoeanna@gmail.com>
2023-02-23 15:37:04 +0100

    elso

--- src/metodusok/Metodusok.java
+++ src/metodusok/Metodusok.java
@@ -10,11 +10,30 @@ package metodusok;
  */
 public class Metodusok {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args, int osszeg) {
+        String kimenet ="Elso 10 szam osszege:"+elso10szamosszege();
+        kiir(kimenet);
+        
+        
+        int szam1 =4, szam2= 5;
+        osszeg = osszead(szam1,szam2);
+        kiir("%d +%+d = %d\n".formatted(szam1,szam2,osszeg));
+    }
+    public static int elso10szamosszege() {
+      int osszeg = 0;
+        for (int i = 0; i < 10; i++) {
+            osszeg += i;
+            
+        }
+        return osszeg;
+    }
+
+   
+    public static int osszead(int a,int b){
+        return a +b;
+    }
+    public static void kiir(String szoveg) {
+        System.out.println(szoveg);
     }
     
 }
