9de60ca85b5057268e4a5685a79a22bcde339286
Wendesday36 <bertazoeanna@gmail.com>
2023-02-23 16:54:26 +0100

    double eq

--- src/metodusok/Equalizer.java
+++ src/metodusok/Equalizer.java
@@ -4,15 +4,46 @@
  */
 package metodusok;
 
+import java.util.Random;
+
 /**
  *
  * @author berta
  */
+    
 public class Equalizer {
+    private static Random rnd =new Random();
     public static void main(){
-       System.out.println("\u001B[45m" + " ");
+       eq5();
+    }
+    public static void eq5(){
+            eq();
+            eq();
+            eq();
+            eq();
+            eq();
+            
     }
     public static void eq(){
+        int db= rnd.nextInt(3,8);
+        eq(db);
         
+    }  
+    private static void eq(int hossz){
+       int db= hossz;
+        String szin = "\u001B[45m";
+        for (int i = 0; i < db; i++) {
+            System.out.println(szin + " ");
     }
+        System.out.println("");
+    }
+    private static void eq(){
+        boolean True;
+        if (true){
+            eq(0);
+    }
+    
+    private static void eq(){
+        eq(true);
+       
 }
